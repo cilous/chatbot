ed72838dbca25b68b0c4fbd5f85ee8f303fbad04
Temwalker <sutepnopnatee@gmail.com>
2018-05-08 16:42:05 +0700

    login page fix auto register / fix firebase add users data

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-plugin-buildinfo.BuildInfo",
+    "file": "plugins/cordova-plugin-buildinfo/www/buildinfo.js",
+    "pluginId": "cordova-plugin-buildinfo",
+    "clobbers": [
+      "BuildInfo"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-buildinfo": "2.0.1"
 };
 // BOTTOM OF METADATA
 });
--- www/js/login.js
+++ www/js/login.js
@@ -1,3 +1,12 @@
+var database = firebase.database();
+function writeUserData(uid,name) {
+  firebase.database().ref('users/' +name).set({
+    uid : uid
+  });
+}
+
+
+
 $('#login').on('click', async function(){
     console.log("click")
     username = $('#username').val() + "@mail.com"
@@ -8,14 +17,21 @@ $('#login').on('click', async function(){
         console.log("login failed")
         // alert(errorMessage)
         // สร้าง user ใหม่ใน database
+        firebase.auth().createUserWithEmailAndPassword(username.toString(), password ).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ...
+        });
+        var uid = firebase.auth().currentUser.uid
+        writeUserData(uid,$('#username').val());
     })
-    console.log("login success")
+    console.log("login success");
     window.location = 'chatroom.html?name=' + $('#username').val();
-    
+
     // ทำการดึงข้อมูลใน database
 })
 
 // หาวิธีดักจับแยกให้ออก ระหว่าง user ที่มีใน database และ ไม่มีใน database
 // ถ้าไม่มีจะเข้า catch ด้านบน แต่ถ้ามีจะวิ่งผ่านมาเลย
 // ประเด็นคือ ถ้าไม่มีแล้วโดน catch มันจะทำบรรทัดล่างก่อน ไม่ยอมรอข้างบนจนเสร็จ
-
